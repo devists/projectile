969d3150841ef2f139c1389b9259b02523b8f9e3
DeepakGoyal468 <goyaldeepu468@gmail.com>
2016-10-28 00:51:19 +0530

    user login and registration completed

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,21 @@
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
+from django.contrib import auth
+from django.contrib.auth import authenticate, login, logout
+from django.views import generic
+from django.views.generic import View
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
 from django.contrib import messages
 from .forms import RegistrationForm, ProfileForm
 from django.contrib.auth.models import User
-from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Create your views here.
+def initiate(request):
+    return render(request,'mainapp/start.html',{})
+
+
 def user_register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST) #This will be used in POST request
@@ -22,7 +32,7 @@ def user_register(request):
             profile.user = user
             user.save()
             profile.save()
-            return HttpResponse("Successfull")
+            return render(request,'mainapp/user_login.html',{})
         else:
             messages.error(request, "Error")
 
@@ -32,18 +42,26 @@ def user_register(request):
         form_pro = ProfileForm()
     return render(request, 'mainapp/user_reg.html', {'form': form, 'form_pro': form_pro})
 
-"""
-def user_login(request):
-    if request.method == "POST":
-        username = request.POST.get("username")
-        passwd = request.POST.get("password")
-        user = authenticate(username=username, password=passwd)
-        if user is not None:
-            HttpResponse("Login Successfull")
-        else:
-            messages.error(request, "username and password did not match")
-            return render(request, "mainapp/tempo.html")
 
-    # for a GET request
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            if user.is_active:
+                login(request,user)
+                return render(request,'mainapp/user_logout.html',{})
+
+        else:
+            return HttpResponse("You dont have an account on this, Please sign up first")
     else:
-        return render(request, "mainapp/tempo.html")"""
+        return render(request, 'mainapp/user_login.html', {})
+
+
+def user_logout(request):
+    logout(request)
+    if request.method == 'POST':
+        return redirect(user_login)
+
