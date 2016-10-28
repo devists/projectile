a56e8081cb493be6b179a7dc1950a965ac740024
jaykishan1 <jayakishan100@gmail.com>
2016-10-28 23:18:53 +0530

    Login Form

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
 from .models import Student
-from django.contrib.admin.widgets import AdminDateWidget
 from django.contrib.auth.forms import AuthenticationForm
 
 
