1211206b0765bb0153faae6ef42accbb375be8ea
AnshulDawar <anshuldawar1995@gmail.com>
2016-11-14 11:14:02 +0530

    for deploy

--- projectile/settings.py
+++ projectile/settings.py
@@ -119,14 +119,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
 # DISABLE_COLLECTSTATIC=1
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../myapp/static'),
+   # os.path.join(BASE_DIR, '../myapp/static'),
 )
 
 LOGIN_REDIRECT_URL = '/'
